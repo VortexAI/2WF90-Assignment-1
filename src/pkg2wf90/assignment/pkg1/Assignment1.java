d10ee8b41c618f370f63df2e5d8873eecb795980
VortexAI <s.c.a.beukers@student.tue.nl>
2018-09-06 16:53:09 +0200

    intialized main class

--- src/pkg2wf90/assignment/pkg1/Assignment1.java
+++ src/pkg2wf90/assignment/pkg1/Assignment1.java
@@ -5,9 +5,9 @@
  */
 package pkg2wf90.assignment.pkg1;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 /**
  *
@@ -17,8 +17,33 @@ public class Assignment1 {
 
     private void run(String[] args) throws FileNotFoundException{
         String file = args[1];
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        
+        Scanner sc = new Scanner(new FileReader(file));
+        Function func;
+        while(sc.hasNext()){
+            String a = sc.nextLine();
+            if(a.startsWith("[radix]")){ 
+                
+                int radix = Integer.parseInt(a.split(" ")[1]);
+                String Assignment = sc.nextLine();
+                String x = sc.nextLine().split(" ")[1];
+                String y = sc.nextLine().split(" ")[1];
+                if(Assignment.equals("[add]")){
+                    func = new AddSub();    
+                } else if(Assignment.equals("[subtract]")){
+                    func = new AddSub();
+                } else if(Assignment.equals("[multiply]")){
+                    func = new EzMult();
+                } else if(Assignment.equals("[karatsuba]")){
+                    func = new Karatsuba();
+                } else {
+                    func = new Euclid();
+                }
+                
+                Number num1 = new Number(x, radix, !x.startsWith("-"));
+                Number num2 = new Number(y, radix, !y.startsWith("-"));
+                func.run(num1, num2);
+            } 
+        }
     }
     
     public static void main(String[] args) throws FileNotFoundException {
