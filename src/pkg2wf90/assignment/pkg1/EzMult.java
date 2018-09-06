089a77f7b0d42570881ff7d9394959f7723e4a94
VortexAI <s.c.a.beukers@student.tue.nl>
2018-09-06 12:09:27 +0200

    didn't save i guess

--- src/pkg2wf90/assignment/pkg1/EzMult.java
+++ src/pkg2wf90/assignment/pkg1/EzMult.java
@@ -11,8 +11,9 @@ package pkg2wf90.assignment.pkg1;
  */
 public class EzMult extends Function{
 
+
     @Override
-    void run(int[] i, int[] j) {
+    void run(Number a, Number b) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
