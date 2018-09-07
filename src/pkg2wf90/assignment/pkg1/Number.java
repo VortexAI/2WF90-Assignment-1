712c612b23fcda4e9ebc45229b47e50b238cb971
VortexAI <s.c.a.beukers@student.tue.nl>
2018-09-07 22:23:28 +0200

    fixed some issues in assignment 1 and added functionality to number

--- src/pkg2wf90/assignment/pkg1/Number.java
+++ src/pkg2wf90/assignment/pkg1/Number.java
@@ -5,7 +5,9 @@
  */
 package pkg2wf90.assignment.pkg1;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
 
 
 /**
@@ -13,14 +15,36 @@ import java.util.*;
  * @author s151810
  */
 public class Number {
+    private final char[] libInt = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private static final Map<Character, Integer> libChar = new HashMap<Character, Integer>();
     char[] number;
     int[] value;
     int radix;
     boolean positive;
     
+    static{
+        libChar.put('0', 0);
+        libChar.put('1', 1);
+        libChar.put('2', 2);
+        libChar.put('3', 3);
+        libChar.put('4', 4);
+        libChar.put('5', 5);
+        libChar.put('6', 6);
+        libChar.put('7', 7);
+        libChar.put('8', 8);
+        libChar.put('9', 9);
+        libChar.put('a', 10);
+        libChar.put('b', 11);
+        libChar.put('c', 12);
+        libChar.put('d', 13);
+        libChar.put('e', 14);
+        libChar.put('f', 15);
+    }
+    
     Number(String number, int radix, boolean positive){
         this.number = number.toCharArray();
-        
+        this.radix = radix;
+        this.positive = positive;
     }
     
     Number(int[] value, int radix, boolean positive){
@@ -31,13 +55,31 @@ public class Number {
     }
     
     public void stringToIntArr(int l){
-        
-        
+        int[] result = new int[l];
+        for(int i : result){
+            i = 0;
+        }
+        for(int i = l-1; i>= l- number.length; i--){
+            result[i] = libChar.get(number[i-(l-number.length)]);
+        }
+        this.value = result;
     }
     
     public char[] intToStringArr(int[] a){
         
         char[] result = new char[0];
+        boolean NonZero = true;
+        
+        for(int i = 0; i < a.length; i++){
+            if(i == 0 && NonZero){
+                
+            } else if(i != 0 && NonZero){
+                result[i] = libInt[i];
+                NonZero = false;
+            } else {
+                result[i] = libInt[i];
+            }
+        }
         
         return result;
     }
