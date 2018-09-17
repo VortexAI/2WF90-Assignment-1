6f807efe966c5251305f14d0fe5a90ace7737731
s166268 <s166268@s166268.campus.tue.nl>
2018-09-17 22:52:57 +0200

    Fixed fault

--- src/pkg2wf90/assignment/pkg1/Euclid.java
+++ src/pkg2wf90/assignment/pkg1/Euclid.java
@@ -19,6 +19,11 @@ public class Euclid extends Function{
     }
     
     Number euclid(Number num1, Number num2) {
+        if (num2.getChars().length > num1.getChars().length) {
+            Number temp = num2;
+            num2 = num1;
+            num1 = temp;
+        }
         num1.stringToIntArr(num1.getChars().length);
         num2.stringToIntArr(num2.getChars().length);
         AddSub subtract = new AddSub(false);
@@ -36,21 +41,54 @@ public class Euclid extends Function{
         Number neg = new Number(oneArray, num1.getRadix(), false);
         Number q;
         Number r;
-        while (num2.thisBiggerThan(zero)) {
+        while (num2.isPositive()) {
             System.out.println("loop");
             q = division(num1, num2);
+            
+            num1.intToStringArr(num1.getIntArr());
+            System.out.print("num1: ");
+            for (int i = 0; i <= num1.getChars().length - 1 ; i++) {
+                System.out.print(num1.getChars()[i]);  
+            }
+            System.out.println();
+            num1.stringToIntArr(num1.getChars().length);
+            
+            num2.intToStringArr(num2.getIntArr());
+            System.out.print("num2: ");           
+            for (int i = 0; i <= num2.getChars().length - 1 ; i++) {
+                System.out.print(num2.getChars()[i]);  
+            }
+            System.out.println();
+            num2.stringToIntArr(num2.getChars().length);
+            
+            q.intToStringArr(q.getIntArr());
+            System.out.print("q: ");             
+            for (int i = 0; i <= q.getChars().length - 1 ; i++) {
+                System.out.print(q.getChars()[i]);  
+            }
+            System.out.println();
+            q.stringToIntArr(q.getChars().length);
+            
             r = subtract.sub(num1, mult.mult(q, num2, true));
+            
+            r.intToStringArr(r.getIntArr());
+            System.out.print("new r: ");             
+            for (int i = 0; i <= r.getChars().length - 1 ; i++) {
+                System.out.print(r.getChars()[i]);  
+            }
+            System.out.println();
+            r.stringToIntArr(r.getChars().length);
+            
             r = removeZeros(r);
             num1 = num2;
             num2 = r;
-            System.out.println(num2.getIntArr()[0]);
+            
             x3 = subtract.sub(x1, mult.mult(q, x2, true));
             y3 = subtract.sub(y1, mult.mult(q, y2, true));
             x1 = x2;
             y1 = y2;
             x2 = x3;
-            y2 = y3;           
-            
+            y2 = y3;          
         }
         Number d;
         d = num1;
@@ -69,6 +107,7 @@ public class Euclid extends Function{
             y = y1;
         }
         d.intToStringArr(d.getIntArr());
+        d.setD();
         return d;
     }
     
@@ -77,6 +116,10 @@ public class Euclid extends Function{
         int[] b = num2.getIntArr();
         int k = a.length;
         int l = b.length;
+        if (k < l) {
+            int[] zero = new int[] {0};
+            return new Number(zero, num1.getRadix(), true);
+        }
         int[] r = new int[k+1];
         for (int i=k; i >= 1; i--) {
             r[i] = a[i-1];
@@ -85,6 +128,7 @@ public class Euclid extends Function{
         int[] q = new int[k-l + 1];
         int base = num1.getRadix();
         int carry;
+        double carryDouble;
         int tmp;
         
         for(int i = k - l; i >= 0; i--) {
@@ -95,7 +139,8 @@ public class Euclid extends Function{
             carry = 0;
             for(int j=0; j <= l-1; j++) {
                 tmp = r[r.length-1 - (i+j)] - q[q.length-1 - i]*b[b.length-1 - j] + carry;
-                carry = tmp/base;
+                carryDouble = Math.floor((double) tmp/base);
+                carry = (int) carryDouble;
                 r[r.length-1 - (i+j)] = tmp%base; 
                 if (r[r.length-1 - (i+j)] < 0) {
                     r[r.length-1 - (i+j)] += base;
@@ -106,7 +151,7 @@ public class Euclid extends Function{
             while (r[r.length-1 - (i+l)] < 0) {
                 carry = 0;
                 for (int j = 0; j <= l-1; j++) {
-                    tmp = r[r.length-1 - (i+j)] + b[b.length-1 - i] + carry;
+                    tmp = r[r.length-1 - (i+j)] + b[b.length-1 - j] + carry;
                     carry = tmp/base;
                     r[r.length-1 - (i+j)] = tmp%base; 
                     if (r[r.length-1 - (i+j)] < 0) {
@@ -118,10 +163,10 @@ public class Euclid extends Function{
             }
         }
         for(int i = q.length-1; i>=0; i--) {
-            System.out.println("q: " + q[i]);
+            //System.out.println("q: " + q[i]);
 	}
         for(int i = r.length-1; i >= 0; i--) {
-            System.out.println("r: " + r[i]);
+            //System.out.println("r: " + r[i]);
         }
         Number result = new Number(q, num1.getRadix(), true);
         return  result;
